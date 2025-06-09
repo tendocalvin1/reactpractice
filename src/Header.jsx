01050cde8207c3615a7624b6923147a4adb6a979
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-06-09 16:38:12 +0300

    Updated Components: Footer, Food and Card

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 
 function Header(){
@@ -18,4 +19,27 @@ function Header(){
     );
 }
 
+=======
+
+
+function Header(){
+    
+    return(
+        <Header>
+            <h1>My first React Website</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </Header>
+        
+    );
+}
+
+>>>>>>> cd9ba20 ( Updated Component: Header)
 export default Header
