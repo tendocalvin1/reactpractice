183484ccaf596f7833b795d22cb16570ca95c7a7
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-06-09 17:29:57 +0300

    Added Button Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Food from './Food.jsx'
 import Card from './Card.jsx'
+import Button from './Button.jsx'
+
+
+
 
 function App() {
       return (
         <>
+      <button></button>
       <Header></Header>
       <Food></Food>
       <Card></Card>
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 function Header(){
@@ -19,27 +18,5 @@ function Header(){
     );
 }
 
-=======
 
-
-function Header(){
-    
-    return(
-        <Header>
-            <h1>My first React Website</h1>
-            <nav>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </nav>
-            <hr></hr>
-        </Header>
-        
-    );
-}
-
->>>>>>> cd9ba20 ( Updated Component: Header)
 export default Header
