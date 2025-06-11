27728de13e58735a4fedddc6f8b78476c56b08d8
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-06-11 13:03:49 +0300

    Made a few changes to the RenderLists.jsx file to perform certain operations like descending and ascending order while classifying fruits

--- src/RenderLists.jsx
+++ src/RenderLists.jsx
@@ -11,12 +11,27 @@ function Lists(){
 // Fruits.sort((a,b)=>a.name.localeCompare(b.name));  // Alphabetical Order
 // Fruits.sort((a,b)=>b.name.localeCompare(a.name));  // Reverse Alphabetical Order.
 // Fruits.sort((a,b)=>a.calories - b.calories);  // Numeric Orders
-Fruits.sort((a,b)=>b.calories - a.calories);   // Reverse Numeric Orders
-const ListItems = Fruits.map(fruit =>  <li key={fruit.id}>
-                                        {fruit.name}: &nbsp;
-                                        <b>{fruit.calories}</b></li>)
+Fruits.sort((a,b)=>b.calories - a.calories); // Reverse Numeric Orders
 
-return (<ol>{ListItems}</ol>)
+
+//const LowCalFruits = Fruits.filter(fruit =>Fruits.calories < 100)
+const HighCalFruits = Fruits.filter(fruit =>Fruits.calories > 100)
+
+const ListItems = HighCalFruits.map(HighCalfruit =>  <li key={HighCalfruit.id}>
+                                      {HighCalfruit.name}: &nbsp;
+                                       <b>{HighCalfruit.calories}</b></li>)
+
+
+
+//const ListItems = LowCalFruits.map(LowCalfruit =>  <li key={LowCalfruit.id}>
+//                                                    {LowCalfruit.name}: &nbsp;
+//                                                    <b>{LowCalfruit.calories}</b></li>)
+
+//const ListItems = Fruits.map(fruit =>  <li key={fruit.id}>
+//                                       {fruit.name}: &nbsp;
+//                                       <b>{fruit.calories}</b></li>)
+
+return (<ol>{ListItems}</ol>);
 }
 
 export default Lists
